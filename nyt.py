ae2b97f00e769b89f62d7374c963039ba1ce5e0e
rbkahn <rbkahn@stanford.edu>
2019-11-13 15:13:07 -0800

    commit

---
+++ nyt.py
@@ -0,0 +1,163 @@
+import pandas as pd
+import pprint
+import requests
+import os
+import sys
+import signal
+from time import sleep
+from datetime import date, timedelta
+from yanytapi.search import TooManyRequestsException
+from bs4 import BeautifulSoup
+from yanytapi import SearchAPI
+api = SearchAPI("TjGk9kxFO9ScvfSF8AfeqkXjjujBnz6e")
+
+def signal_handler(sig, frame):
+    print(str(date))
+    print(articles_skipped, " articles skipped")
+    df.to_csv(csv_name)
+    print('You pressed Ctrl+C!')
+    sys.exit(0)
+
+
+
+signal.signal(signal.SIGINT, signal_handler)
+articles_skipped = 0
+first_date = date.today()
+our_date = first_date
+already_errored = False
+
+csv_name = 'articles-' + str(our_date.year) + '.csv'
+if os.path.exists(csv_name):
+    df = pd.read_csv(csv_name)
+    id_set = set(df['id'])
+    date_set = set(df['date'].apply(lambda x : pd.to_datetime(x).date()))
+else:
+    df = pd.DataFrame()
+    id_set = set()
+    date_set = set()
+article_count = 0
+
+while(True):
+    if our_date not in date_set:
+        for term in ["cannabis", "marijuana"]:
+            try: 
+                date_str = str(our_date).replace('-', '')
+                print(str(our_date))
+                sleep(6)
+                articles = api.search(term, begin_date=date_str, end_date=date_str)
+                for article in articles:
+                    if article._id in id_set:
+                        articles_skipped += 1
+                        continue
+                    id_set.add(article._id)
+                    article_count += 1
+                    print(article_count, article.headline['main'], article.pub_date)
+                    session = requests
+                    url = article.web_url
+                    req = session.get(url)
+                    soup = BeautifulSoup(req.content, 'html.parser')
+                    paragraphs = soup.find_all('p')
+                    count = 0
+                    for p in paragraphs:
+                        text = p.get_text()
+                        if len(text.split()) < 5:
+                            continue
+                        count += 1
+                        df = df.append({'id': article._id, 
+                                'title': article.headline['main'], 
+                                'date': article.pub_date,
+                                'url': article.web_url,
+                                'p#' : count,
+                                'text': text}, ignore_index=True)
+                    if article_count % 50 == 0:
+                        df.to_csv(csv_name)
+            except TooManyRequestsException:
+                print("Too many requests")
+                raise
+            except ConnectionError:
+                print("Conection error")
+                sleep(6)
+                continue
+            except:
+                if already_errored:
+                    print(our_date)
+                    print(articles_skipped, " articles skipped")
+                    df.to_csv(csv_name)
+                    import sys; print("Unexpected error:", sys.exc_info()[0])
+                    raise    
+                else:
+                    already_errored = True
+                    continue
+    date_set.add(our_date)
+    our_date -= timedelta(days=1)
+    if (our_date.month == 1 and our_date.day == 1):
+        print("Done with ", our_date.year)
+        df.to_csv(csv_name)
+        our_date -= timedelta(days=1)
+        csv_name = 'articles-' + str(our_date.year) + '.csv'
+        if os.path.exists(csv_name):
+            df = pd.read_csv(csv_name)
+            id_set = set(df['id'])
+            date_set = set(df['date'].apply(lambda x : pd.to_datetime(x).date()))
+        else:
+            df = pd.DataFrame()
+            id_set = set()
+            date_set = set()
+        article_count = 0
+    already_errored = False
+
+'''
+for year in range(2019, 1985, -1):
+    if year == 2019:
+        sdate = datetime(2019, 11, 1)
+    else:
+        sdate = datetime(2019, 12, 31)
+    csv_name = 'articles-' + str(year) + '.csv'
+    if os.path.exists(csv_name):
+        df = pd.read_csv(csv_name)
+        id_set = set(df['id'])
+    else:
+        df = pd.DataFrame()
+        id_set = set()
+    for begin, end in [('0201', '0228'), ('0301', '0331'), ('0401', '0430'), ('0501', '0531'), ('0601', '0630'), ('0701', '0731'), ('0801', '0831'), ('0901', '0930'), ('1001', '1031'), ('1101','1130'), ('1201', '1231'), ('0101', '0131'), ]:
+        for term in ["cannabis", "marijuana"]:
+            try: 
+                begin_date = str(year) + begin
+                end_date = str(year) + end
+                articles = api.search(term, begin_date=begin_date, end_date=end_date)
+                article_count = 0
+                for article in articles:
+                    if article._id in id_set:
+                        articles_skipped += 1
+                        continue
+                    id_set.add(article._id)
+                    article_count += 1
+                    print(article_count, article.headline['main'], article.pub_date)
+                    session = requests
+                    url = article.web_url
+                    req = session.get(url)
+                    soup = BeautifulSoup(req.content, 'html.parser')
+                    paragraphs = soup.find_all('p')
+                    count = 0
+                    for p in paragraphs:
+                        text = p.get_text()
+                        if len(text.split()) < 5:
+                            continue
+                        count += 1
+                        df = df.append({'id': article._id, 
+                                'title': article.headline['main'], 
+                                'date': article.pub_date,
+                                'url': article.web_url,
+                                'p#' : count,
+                                'text': text}, ignore_index=True)
+                    
+                    if article_count % 50 == 0:
+                        df.to_csv(csv_name)
+            except:
+                print(year, begin, end)
+                print(articles_skipped, " articles skipped")
+                df.to_csv(csv_name)
+                import sys; print("Unexpected error:", sys.exc_info()[0])
+                raise
+'''
+    
